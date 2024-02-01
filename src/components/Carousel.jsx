c5bdf16933a2ec3a0bce0a54d80e01ddf8251ca5
riserishitha <rishitha.n@kalvium.community>
2024-02-01 12:04:27 +0530

    changes

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,13 +1,33 @@
 import React, { useState } from "react";
 import "./Carousel.css";
 import { images } from "../data/CarouselData";
-
-// you can research - how to use material ui
-import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
-// complete the function below:
 function Carousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleIncrease = () => {
+    setIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
+  };
+
+  const handleDecrease = () => {
+    setIndex((prevIndex) => (prevIndex === 0 ? images.length - 1 : prevIndex - 1));
+  };
+
+  const currentImage = images[index];
+
+  return (
+    <div className="flex">
+      <div className="image-info">
+        <h2>{currentImage.title}</h2>
+        <p>{currentImage.subtitle}</p>
+      </div>
+      <ArrowBackIosIcon onClick={handleDecrease} className="arrows" />
+      <img src={currentImage.img} alt="" />
+      <ArrowForwardIosIcon onClick={handleIncrease} className="arrows" />
+    </div>
+  );
 }
 
 export default Carousel;
